8695d30a43cf9c18a55f348eefa0c2896e40bcc1
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 17:04:34 +0900

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 import pytube  
 try:
         video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
-            youtube = pytube.YouTube(video_url)  
+            youtube = youube.YouTube(video_url)  
                 video = youtube.streams.first()  
                     video.download('C:/Windows/System32/httpd-2.4.52-win64-VS16/Apache24/test/youtube')  
                         print("Download Successfull !!")
