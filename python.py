8309ff1eb4c84c94cd3a65dc6ea1090e390760bb
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 21:12:53 +0900

    python py directory change 3

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 import pytube  
 try:
         video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
-            youtube = youube.YouTube(video_url)  
+            youtube = pytube.YouTube(video_url)  
                 video = youtube.streams.first()  
                     video.download('c:/'Programs Files (x86)'/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
                         print("Download Successfull !!")
