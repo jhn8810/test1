cbd7741390f3001f9e7cdff7947527846227a9b9
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 20:59:44 +0900

    mv apache reposit commit

--- python.py
+++ python.py
@@ -1,10 +1,10 @@
 from pytube import YouTube  
 import pytube  
 try:
-        video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
-            youtube = pytube.YouTube(video_url)  
-                video = youtube.streams.first()  
-                    video.download('C:/Windows/System32/httpd-2.4.52-win64-VS16/Apache24/test/youtube')  
-                        print("Download Successfull !!")
+    video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
+    youtube = pytube.YouTube(video_url)  
+    video = youtube.streams.first()  
+    video.download('C:/Windows/System32/httpd-2.4.52-win64-VS16/Apache24/test/hello-git/loc-git/youtube')  
+    print("Download Successfull !!")
 except:
-        print("Something Went Wrong !!")
+    print("Something Went Wrong !!")
