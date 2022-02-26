bec171d36fec4f8ae6d70851dc52199411039c9e
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 21:08:21 +0900

    python py directory change

--- python.py
+++ python.py
@@ -1,18 +1,10 @@
 from pytube import YouTube  
 import pytube  
 try:
-<<<<<<< HEAD
-    video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
-    youtube = pytube.YouTube(video_url)  
-    video = youtube.streams.first()  
-    video.download('C:/Windows/System32/httpd-2.4.52-win64-VS16/Apache24/test/hello-git/loc-git/youtube')  
-    print("Download Successfull !!")
-=======
         video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
             youtube = youube.YouTube(video_url)  
                 video = youtube.streams.first()  
-                    video.download('C:/Windows/System32/httpd-2.4.52-win64-VS16/Apache24/test/hello-git/loc-git/youtube')  
+                    video.download('c:/Programs Files (x86)/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
                         print("Download Successfull !!")
->>>>>>> cd3548cc3623212d11c3fa48a57fb0c7f3738a20
 except:
     print("Something Went Wrong !!")
