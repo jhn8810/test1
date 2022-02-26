d0214b2704af190205c41473692c96ecbdc5c9a8
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 21:10:18 +0900

    python py directory change 2

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ try:
         video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
             youtube = youube.YouTube(video_url)  
                 video = youtube.streams.first()  
-                    video.download('c:/Programs Files (x86)/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
+                    video.download('c:/'Programs Files (x86)'/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
                         print("Download Successfull !!")
 except:
     print("Something Went Wrong !!")
