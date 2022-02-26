652d820112e521987faa09ec6a071c17cf522eef
jhn8810 <100028843+jhn8810@users.noreply.github.com>
2022-02-26 21:31:41 +0900

    cuga

--- python.py
+++ python.py
@@ -2,9 +2,9 @@ from pytube import YouTube
 import pytube  
 try:
         video_url = 'https://www.youtube.com/watch?v=lTTajzrSkCw'   
-            youtube = pytube.YouTube(video_url)  
-                video = youtube.streams.first()  
-                    video.download('c:/Programs Files (x86)/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
-                        print("Download Successfull !!")
+        youtube = pytube.YouTube(video_url)  
+        video = youtube.streams.first()  
+        video.download('c:/Program Files (x86)/httpd-2.4.52-win64-VS16/Apache24/hello-git/loc-git/youtube')  
+        print("Download Successfull !!")
 except:
     print("Something Went Wrong !!")
